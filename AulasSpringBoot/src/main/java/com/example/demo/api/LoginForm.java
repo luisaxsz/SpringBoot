087863ab479732f7e54693953553f969719f28d3
Maria Luisa Lameirão Sousa <marialuisalameirao@gmail.com>
2023-07-24 14:09:51 -0300

    Aula de Criação de rotas na api

--- AulasSpringBoot/src/main/java/com/example/demo/api/LoginForm.java
+++ AulasSpringBoot/src/main/java/com/example/demo/api/LoginForm.java
@@ -1,4 +1,4 @@
-package com.example.Carros.api;
+package com.example.demo.api;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
