1d791f469afe8d6af986e383f640bcff56b6fec1
Maria Luisa Lameirão Sousa <marialuisalameirao@gmail.com>
2023-07-24 13:21:22 -0300

    Parâmetros por PathVariable

--- Carros/src/main/java/com/example/Carros/api/IndexController.java
+++ Carros/src/main/java/com/example/Carros/api/IndexController.java
@@ -2,6 +2,7 @@ package com.example.Carros.api;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,11 @@ public class IndexController {
 	//Post - Mais recomendado
 	@GetMapping("/login")
 	public String login(@RequestParam("login") String login, @RequestParam("senha") String senha) {
+		return " login:  " + login + "  senha: " + senha;
+	}
+	
+	@GetMapping("/login2/login/{login}/senha/{senha}")
+	public String login2(@PathVariable("login") String login, @PathVariable("senha") String senha) {
 		return " login:  " + login + "  senha: " + senha;
 	}
 	
