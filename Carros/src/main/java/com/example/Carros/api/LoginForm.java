1fc951ad1387ce135ed1dd063e9051bdf9c4e8c8
Maria Luisa Lameirão Sousa <marialuisalameirao@gmail.com>
2023-07-24 13:40:18 -0300

    PassagemParametros

--- Carros/src/main/java/com/example/Carros/api/LoginForm.java
+++ Carros/src/main/java/com/example/Carros/api/LoginForm.java
@@ -12,29 +12,35 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/")
 public class LoginForm {
-	//Post - Mais recomendado
-		@GetMapping("/login")
-		public String login(@RequestParam("login") String login, @RequestParam("senha") String senha) {
-			return " login:  " + login + "  senha: " + senha;
-		}
-		
-		@GetMapping("/login2/login/{login}/senha/{senha}")
-		public String login2(@PathVariable("login") String login, @PathVariable("senha") String senha) {
-			return " login:  " + login + "  senha: " + senha;
-		}
-		
-		@PostMapping()
-		public String post() {
-			return "Post SpringBoot";
-		}
-		
-		@PutMapping()
-		public String put() {
-			return "PUT SpringBoot";
-		}
-		
-		@DeleteMapping()
-		public String delete() {
-			return "Delete SpringBoot";
-		}
+	// Post - Mais recomendado
+	@GetMapping("/login")
+	public String login(@RequestParam("login") String login, @RequestParam("senha") String senha) {
+		return " login:  " + login + "  senha: " + senha;
+	}
+
+	@GetMapping("/login2/login/{login}/senha/{senha}")
+	public String login2(@PathVariable("login") String login, @PathVariable("senha") String senha) {
+		return " login:  " + login + "  senha: " + senha;
+	}
+
+	@GetMapping("/Carros/{id}")
+	public String getCarroById(@PathVariable("id") Long id) {
+		return "Id: " + id;
+	}
+	
+
+	@PostMapping("/loginPost")
+	public String loginPost(@RequestParam("login") String login, @RequestParam("senha") String senha) {
+		return " login:  " + login + "  senha: " + senha;
+	}
+
+	@PutMapping()
+	public String put() {
+		return "PUT SpringBoot";
+	}
+
+	@DeleteMapping()
+	public String delete() {
+		return "Delete SpringBoot";
+	}
 }
