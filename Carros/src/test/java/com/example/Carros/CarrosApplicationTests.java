f537bbadbe0a97b3ca4a1b5fc9bab93d4fa739ee
Maria Luisa Lameirão Sousa <marialuisalameirao@gmail.com>
2023-07-26 14:52:14 -0300

    Testes unitários no springBoot

--- Carros/src/test/java/com/example/Carros/CarrosApplicationTests.java
+++ Carros/src/test/java/com/example/Carros/CarrosApplicationTests.java
@@ -1,13 +1,60 @@
 package com.example.Carros;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.util.Assert;
+
+import com.Carros.CarrosApplication;
+import com.Carros.domain.Carro;
+import com.Carros.domain.CarrosService;
+import com.Carros.domain.dto.CarroDTO;
 
 @SpringBootTest
-class CarrosApplicationTests {
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = CarrosApplication.class)
+public class CarrosApplicationTests {
+	
+	@Autowired
+	private CarrosService service;
 
 	@Test
-	void contextLoads() {
+	public void test1() {
+		Carro carro = new Carro();
+		carro.setNome("Ferrari");
+		carro.setTipo("Luxo");
+		
+		CarroDTO c = service.insert(carro);
+		
+		assertNotNull(c);
+		
+		Long id = c.getId();
+		assertNotNull(id);
+		
+		//Buscar Objeto
+		Optional <CarroDTO> op = service.getCarroById(id);
+		assertTrue(op.isPresent());
+		
+		c = op.get();
+		
+		assertEquals("Ferrari", c.getNome());
+		assertEquals("Luxo", c.getTipo());
+		
+		//Deletar objeto
+		service.delete(id);
+		
+		//Verificar se deletou
+		assertFalse(service.getCarroById(id).isPresent());
 	}
 
 }
