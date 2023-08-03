be2f5169bbb85ffd66e930a33327b1b1a22a26eb
Maria Luisa Lameirão Sousa <marialuisalameirao@gmail.com>
2023-08-03 19:41:20 -0300

    Criações de perfis e autenticação em memória

--- Carros/src/main/java/com/Carros/api/CarrosController.java
+++ Carros/src/main/java/com/Carros/api/CarrosController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -59,6 +60,7 @@ public class CarrosController {
 	}
 
 	@PostMapping
+	@Secured({"ROLE_ADMIN"})
 	public ResponseEntity<?> post(@RequestBody Carro carro) {
 		CarroDTO c = service.insert(carro);
 		URI location = getUri(c.getId());
--- Carros/src/main/java/com/Carros/api/security/SecurityConfig.java
+++ Carros/src/main/java/com/Carros/api/security/SecurityConfig.java
@@ -2,23 +2,48 @@ package com.Carros.api.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
 	@Bean 
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
 		 http
          .authorizeHttpRequests((authz) -> authz
+        	 .requestMatchers("/api/v1/carros").hasRole("ADMIN")
              .anyRequest().authenticated()
          )
          .httpBasic();
-    return http.build();
+    return http.getOrBuild();
 	}
+	@Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();       
+        UserDetails admin = User.withDefaultPasswordEncoder()
+            .username("ADMIN")
+            .password(encoder.encode("ADMIN"))
+            .roles("ADMIN")
+            .build();
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("USER")
+                .password(encoder.encode("USER"))
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(admin, user);
+    }
+
 	/*
 	@Bean
 	public WebSecurityCustomizer webSecurityCustomizer() {
