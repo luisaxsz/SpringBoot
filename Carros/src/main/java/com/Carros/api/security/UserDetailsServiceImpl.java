dad9bf446ff8466f882e28d950258e0f4b9ad47a
Maria Luisa Lameirão Sousa <marialuisalameirao@gmail.com>
2023-08-04 18:20:30 -0300

    interface UserDetailService

---
+++ Carros/src/main/java/com/Carros/api/security/UserDetailsServiceImpl.java
@@ -0,0 +1,20 @@
+package com.Carros.api.security;
+
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+
+public class UserDetailsServiceImpl implements UserDetailsService{
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		return null;
+	}
+
+	
+
+}
