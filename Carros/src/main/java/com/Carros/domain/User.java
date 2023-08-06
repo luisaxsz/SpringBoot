4d3eca9d368af352c0f5900ed6b23df366f42147
Maria Luisa Lameirão Sousa <marialuisalameirao@gmail.com>
2023-08-06 13:12:25 -0300

    Criação roles e associação de user com roles

--- Carros/src/main/java/com/Carros/domain/User.java
+++ Carros/src/main/java/com/Carros/domain/User.java
@@ -1,9 +1,18 @@
 package com.Carros.domain;
 
+import java.util.List;
+
+import org.hibernate.annotations.ManyToAny;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinColumns;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import lombok.Data;
 
 @Entity
@@ -19,4 +28,10 @@ public class User {
 	private String login;
 	private String senha;
 	
+	//Tabela de associação
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "user_roles",
+		joinColumns = @JoinColumn(name="user_id", referencedColumnName = "id"),
+		inverseJoinColumns = @JoinColumn(name= "role_id", referencedColumnName = "id"))
+	private List<Role> roles;
 }
